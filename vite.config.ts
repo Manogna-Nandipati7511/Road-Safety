dfedb730f2ad6fae9e0b0b48e188e5832dc75244
Manognanandipati <manog@example.com>
2025-11-16 18:47:27 +0530

    Updated README with project report content

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Road-Safety/",    // <-- ADD THIS LINE (VERY IMPORTANT)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
